efb21b2caa6dac13bb2bd93e0c76eca2838b3ab4
Anton Dolgopolov <antondolgopolov86@gmail.com>
2024-05-18 12:51:48 +0500

    Add conections to Database in file Util.java. Add properties. Add ferify connection database in file Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.util.Util;
+import org.postgresql.Driver;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        try (Connection connection = Util.getConnection()) {
+            if (connection != null) {
+                System.out.println("Соединение с базой данных установлено успешно.");
+            } else {
+                System.out.println("Не удалось установить соединение с базой данных.");
+            }
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка при попытке подключения к базе данных:");
+
+        }
+
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,47 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = Util.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                System.out.println("файл application.properties не найден");
+
+            } else {
+                properties.load(input);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static final String USERNAME = properties.getProperty("db.username");
+    private static final String PASSWORD = properties.getProperty("db.password");
+    private static final String URL = properties.getProperty("db.url");
+
+    private Util() {
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            // Регистрация драйвера
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            // Получение соединения
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+
+    }
 }
